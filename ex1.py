9a3649630f0cc185fea3ec1acffb2fa2271cb577
Ido Barkan <barkanido@hotmail.com>
2015-11-13 15:20:59 +0200

    - removed missing values (to a different dataframe) - started categorizing columns

--- ex1.py
+++ ex1.py
@@ -7,7 +7,27 @@ import pylab
 working_dir = "./data"
 # For .read_csv, always use header=0 when you know row 0 is the header row
 data_filename = "ElectionsData.csv"
-train = pd.read_csv(path.join(working_dir, data_filename), header=0)
-train.to_csv(path.join(working_dir, data_filename + '-copy'), index=False)
+data_full_path = path.join(working_dir, data_filename)
+train = pd.read_csv(data_full_path, header=0)
+data_copy_full_path = data_full_path + '-copy'
+
+# backup just in case
+train.to_csv(data_copy_full_path, index=False)
+
+
+def categorize(df, columns):
+    for column in columns:
+        df[column + '_categorical'] = df[column].astype('category')
+        df = df.drop(column, 1)
+
+
+# categorize stuff
+categorize(train, ('Vote', 'Married'))
+
+without_missing_data = train.dropna()
+
+# categorize Married
+categorize(without_missing_data, ('Vote', 'Married'))
+
 train.Num_of_kids_born_last_10_years.dropna().hist()
 pylab.show()
